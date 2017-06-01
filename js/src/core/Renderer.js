adda1af6785a4276f30445f4d484720248d5013c
cankillah0 <cankillah@i.ua>
2017-06-01 11:19:25 +0300

    resize handler

--- js/src/core/Renderer.js
+++ js/src/core/Renderer.js
@@ -9,8 +9,17 @@ export class Renderer{
     }
 
     initialize(){
-        this.renderer = PIXI.autoDetectRenderer(window.innerWidth,window.innerHeight);
+
+        var canvas = document.getElementById('canvas');
+        this.renderer = PIXI.autoDetectRenderer(1262, 760, canvas);
         document.body.appendChild(this.renderer.view);
+        this.renderer.view.style.position = 'absolute';
+        this.renderer.view.style.left = '50%';
+        this.renderer.view.style.top = '50%';
+        this.renderer.view.style.transform = 'translate3d( -50%, -50%, 0 )';
+
+        this.ratio = 1262/760;
+
         this._stage = new PIXI.Container();
         this._stage.hitArea = new PIXI.Rectangle(0, 0, 1262, 760);
         this._stage.interactive = true;
@@ -18,6 +27,21 @@ export class Renderer{
         this._onUpdateSignal = new signals.Signal();
         AppProxy.getInstance().stage = this._stage;
         this.animate();
+
+        window.onresize = this.onWindowResize.bind(this);
+        this.onWindowResize(null);
+    }
+
+    onWindowResize(event){
+        if (window.innerWidth / window.innerHeight >= this.ratio) {
+            var w = window.innerHeight * this.ratio;
+            var h = window.innerHeight;
+        } else {
+            var w = window.innerWidth;
+            var h = window.innerWidth / this.ratio;
+        }
+        this.renderer.view.style.width = w/1.2 + 'px';
+        this.renderer.view.style.height = h/1.2 + 'px';
     }
 
     animate(){
